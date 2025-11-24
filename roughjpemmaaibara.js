2fc7e8fbb9fec9f37cf3d11103d8d80f986bb308
Alissa <alissawu77@gmail.com>
2025-11-24 01:35:08 -0500

    prevocal section done! ish - can be improved but sounds good

--- roughjpemmaaibara.js
+++ roughjpemmaaibara.js
@@ -3,33 +3,54 @@ samples('github:tidalcycles/dirt-samples')
   setcpm(160/4) //40 bpm, 4 beats to a bar. one cycle = one bar
 
 // section 1: pre lyric
-let ethereal = note(`<
-  {f#4 d4 a3 [a3 f#3]}
-  {f#4 d4 a3 [a3 f#3]}
-  {[f#4 f#4 f#4 f#4] [d4 d4 d4 d4] [a3 a3 a3 a3] [a3 a3 f#3 f#3]}
-  {[f#4 f#4 f#4 f#4] [d4 d4 d4 d4] [a3 a3 a3 a3] [a3 a3 f#3 f#3]}
->`).sound("gm_pad_warm:6").slow(4).room(1.8).lpf(1200)
+let ethereal = n(`<
+  {f#4 d4 a3 [a3 f#3]}*2
+  {[f#4 f#4 f#4 f#4] [d4 d4 d4 d4] [a3 a3 a3 a3] [a3 a3 f#3 f#3]}*2
+>`).scale("C2:chromatic").sound("gm_pad_warm:6").slow(8).room(1.8).lpf(1200)
   .hpf(200).attack(0.5).release(2).size(0.95)
-  .gain(0.4)
-let bass = n(`<b2 g2 [d2, f#2] [d2, f#2]>`)
-  .scale("C2:chromatic")
+  .gain(0.05)
+
+let bass = n(`<
+{b2 g2 d2 [d2, f#2]}
+{b2 g2 d2 [d2, f#2]}
+{[b2 b2 b2 b2] [g2 g2 g2 g2] [d2 d2 d2 d2] [d2 d2 d2 d2, ~ ~ f#2 f#2]}
+{[b2 b2 b2 b2] [g2 g2 g2 g2] [d2 d2 d2 d2] [d2 d2 d2, f#2 f#2 f#2]}
+>`)
+  .scale("C2:chromatic").slow(4)
   .sound("supersaw").sustain(0.7)
   .release(0.1)
   .lpf("<800 1200 600 1500>*2")
   .room(1.3)
   .gain(0.1).resonance(0.6)
-  ._punchcard()
 
+let vocal = note(`<
+{~ [d4 e4 d4 e4] d4@2}
+{~ [d4 e4 d4 e4] [d4 f#3] [d4 d4]}
+>`)
+  .scale("C2:chromatic").sound("gm_electric_guitar_muted:6")
+  .slow(2).release(0.4).attack(0.2).decay(0.5).lpf(1000).lpq(0.2).sustain(1).vowel("a")
+  .gain(5.2).room(1.3).size(0.7).drive(0.1).pan(tri.slow(2))
 
-// drums that appear at the start ish
-// lpf 100 removes the weird blip sound at the top
-_$:s("rolandtr808_bd:1")
+let drums = s("rolandtr808_bd:1")
   .struct(`<
-    { [[1 1 ~] 1] [1 1] }
-    { [[1 1 ~] 1] [[1 1 1] 1] }
-  >`).gain(0.05).lpf(100)._punchcard()
-// weird shaker sound that appears with the start drums
-_$: s("hr16_sh*4").gain(0.5).delay(0.3, 0.15).room(0.8).sustain(0.5).gain(0.01).resonance(0.6)
+  {0 0 0 0}
+  {0 0 0 0}
+  {0 0 0 0}
+  {0 0 0 0}
+  { [[1 1 ~] 1] [1 1] }
+  { [[1 1 ~] 1] [[1 1 1] 1] }
+  { [[1 1 ~] 1] [1 1] }
+  { [[1 1 ~] 1] [[1 1 1] 1] }
+  >`).gain(0.05).lpf(100).decay(1.1)
+
+let shaker = s("hr16_sh*4").gain(0.5).delay(0.3, 0.15).room(0.8).sustain(0.5).gain(0.01).resonance(0.6)
+
+$: stack(
+  bass, vocal, drums, ethereal
+)
+
+
+
 
 // underlying deep bass
 _$: n(`<b2 g2 [d2, f#2] [d2, f#2]>`)
@@ -40,7 +61,7 @@ _$: n(`<b2 g2 [d2, f#2] [d2, f#2]>`)
   .room(1.3)
   .gain(0.1).resonance(0.6)
   ._punchcard()
-_$: note(`<
+_$:note(`<
 {~ [d4 e4 d4 e4] d4@2}
 {~ [d4 e4 d4 e4] [d4 f#3] [d4 d4]}
 >`)
