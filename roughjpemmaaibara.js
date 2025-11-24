0b35e11ca868f8bf1654937ddd3392eea6fa6633
Alissa <alissawu77@gmail.com>
2025-11-24 01:11:17 -0500

    working on the ordering?

--- roughjpemmaaibara.js
+++ roughjpemmaaibara.js
@@ -2,19 +2,16 @@ samples('github:algorave-dave/samples')
 samples('github:tidalcycles/dirt-samples')
   setcpm(160/4) //40 bpm, 4 beats to a bar. one cycle = one bar
 
-// tu ta too ta (mid hi low hi)
-let complex_drums = s("[tr909_bd:2 tr909_hh:1 tr909_sd:5 tr909_hh:1] [tr909_sd:6 tr909_sd:6] [tr909_bd:3 tr909_sd:7] [tr909_sd:6 tr909_sd:6]")          // push it into breakcore-ish speed
-  .lpf("<1500 1200>").hpf(100)
-  .room(0.4).resonance(0.6).lpf(800)
-
-let basic_drums = s("tr909_sd:5 tr909_hh:1 tr909_bd:2 tr909_hh:1")
-  .delay(0.15).lpf(800)
-$: stack(
-  basic_drums,
-  complex_drums
-)._punchcard()
-// harmony
-$: n(`<b2 g2 f#2 f#2>`)
+// section 1: pre lyric
+let ethereal = note(`<
+  {f#4 d4 a3 [a3 f#3]}
+  {f#4 d4 a3 [a3 f#3]}
+  {[f#4 f#4 f#4 f#4] [d4 d4 d4 d4] [a3 a3 a3 a3] [a3 a3 f#3 f#3]}
+  {[f#4 f#4 f#4 f#4] [d4 d4 d4 d4] [a3 a3 a3 a3] [a3 a3 f#3 f#3]}
+>`).sound("gm_pad_warm:6").slow(4).room(1.8).lpf(1200)
+  .hpf(200).attack(0.5).release(2).size(0.95)
+  .gain(0.4)
+let bass = n(`<b2 g2 [d2, f#2] [d2, f#2]>`)
   .scale("C2:chromatic")
   .sound("supersaw").sustain(0.7)
   .release(0.1)
@@ -22,14 +19,37 @@ $: n(`<b2 g2 f#2 f#2>`)
   .room(1.3)
   .gain(0.1).resonance(0.6)
   ._punchcard()
-//melody
-$: note(`<
+
+
+// drums that appear at the start ish
+// lpf 100 removes the weird blip sound at the top
+_$:s("rolandtr808_bd:1")
+  .struct(`<
+    { [[1 1 ~] 1] [1 1] }
+    { [[1 1 ~] 1] [[1 1 1] 1] }
+  >`).gain(0.05).lpf(100)._punchcard()
+// weird shaker sound that appears with the start drums
+_$: s("hr16_sh*4").gain(0.5).delay(0.3, 0.15).room(0.8).sustain(0.5).gain(0.01).resonance(0.6)
+
+// underlying deep bass
+_$: n(`<b2 g2 [d2, f#2] [d2, f#2]>`)
+  .scale("C2:chromatic")
+  .sound("supersaw").sustain(0.7)
+  .release(0.1)
+  .lpf("<800 1200 600 1500>*2")
+  .room(1.3)
+  .gain(0.1).resonance(0.6)
+  ._punchcard()
+_$: note(`<
 {~ [d4 e4 d4 e4] d4@2}
 {~ [d4 e4 d4 e4] [d4 f#3] [d4 d4]}
 >`)
   .scale("C2:chromatic").sound("gm_overdriven_guitar:10")
-  .slow(2).release(0.4).attack(0.05).lpf(2200).sustain(1)
-  .vowel("a").gain(3).room(0.4).size(0.7)
+  .slow(2).release(0.4).attack(0.2).decay(0.4).lpf(1100).lpq(0.2).sustain(1).vowel("a")
+  .gain(5.2).room(0.8).size(0.7).drive(0.1).pan(tri.slow(2))
   ._punchcard()
 
 
+
+
+// prelyric -- vocal, bass, ethereal sound
