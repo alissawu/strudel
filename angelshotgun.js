ca2d5024e195d27716b13bec4e43b9afa3fe1d35
Alissa <alissawu77@gmail.com>
2025-11-26 03:14:38 -0500

    .

--- angelshotgun.js
+++ angelshotgun.js
@@ -4,7 +4,11 @@ setcpm(30/4)
 // gm_tuba:0 (muffled tuba)
 // gm_fx_atmosphere(13):2 (i think?) - kinda angelic
 // gm_french_horn(5):1 and 5 (the last one. or is it 5?) (distant)
-
+//tr808 1, 6
+let drums = s("tr808_bd:6, [~ sd:1]*2")
+  .struct(`<
+  { 1 [~@7 1] [1 ~] 1}
+  >`).gain(0.2).sustain(0.5).lpf(100)
 let bkgd = n(`<
   {f#4 g4 f#4 e4}
   {[b3, f#4] [g3, g4] [d4, f#4] [a3, e4]}
@@ -13,9 +17,9 @@ let bkgd = n(`<
   .lpenv(-3).pan(cosine.slow(2))
 let front = n(`<
 {~}
-  {~ [f#4 f#4] ~ [g4 g4] ~ [f#4 f#4] e4@2}!2
->`).scale("C2:chromatic").sound("gm_voice_oohs:2").room(2).lpf(500)
-  .hpf(300).attack(0.5).release(1).sustain(1).decay(0.75).gain(0.15)
+  {~ [b3 b3, d4 d4, f#4 f#4, f#5 f#5] ~ [g3 g3, d4 d4, g4 g4, g5 g5] ~ [a3 a3, d4 d4, f#4 f#4, f#5 f#5] [a3, e4]@2}!2
+>`).scale("C2:chromatic").sound("gm_voice_oohs:3").room(2).lpf(500)
+  .hpf(300).attack(0.3).release(1).sustain(1).decay(0.75).gain(0.15)
   .lpenv(-3).pan(sine.slow(2))
 let bass = n(`<
 {b1 g1 d2 c#2}>`)
@@ -31,5 +35,5 @@ let brass = n(`<{a3 a3 a3 c#4}>`).sound("gm_french_horn").scale("C2:chromatic")
   .room(1)
   .gain(0.04).resonance(0.6).pan(sine.slow(2))
 $: stack(
-  brass, bkgd, front, bass
+  brass, bkgd, front, bass, drums
 )
