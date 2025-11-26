88c733b856257f40222d9eb2fef685446f565c9a
Alissa <alissawu77@gmail.com>
2025-11-26 05:51:25 -0500

    alternated backvocal correctly

--- angelshotgun.js
+++ angelshotgun.js
@@ -20,7 +20,7 @@ let front = n(`<
   {~ [f#3 f#3, b3 b3, d4 d4, f#4 f#4, f#5 f#5] ~ [g2 g2, g3 g3, d4 d4, g4 g4, g5 g5] ~ [f#3 f#3, a3 a3, d4 d4, f#4 f#4, f#5 f#5] [e3, a3, e4]@2}
   {[f#2 f#2] [f#3 f#3, b3 b3, d4 d4, f#4 f#4, f#5 f#5] [g2 g2] [g2 g2, g3 g3, d4 d4, g4 g4, g5 g5] [f#2 f#2] [f#3 f#3, a3 a3, d4 d4, f#4 f#4, f#5 f#5] [e3, a3, e4]@2}
 >`).scale("C2:chromatic").sound("gm_fx_atmosphere").room(2).lpf(500)
-  .hpf(300).attack(0.2).release(1).sustain(1).decay(0.75).gain("<0.1 0.1 0.05>")
+  .hpf(300).attack(0.2).release(1).sustain(1).decay(0.75).gain("<0 0.07 0.03>")
   .lpenv(-3).pan(sine.slow(2))
 let bass = n(`<
 {b1 g1 d2 c#2}!2
@@ -44,15 +44,15 @@ let vocal = n(`<
 {~@6 [~@2 e4 e4] [f#4 e4 f#4 a4]}
 {[f#4 e4] ~ ~ [f#4 e4 f#4 a4] [f#4 e4] ~ ~@2}
 >`).sound("gm_violin").scale("C4:chromatic")
-  .release(0.55).attack(0.1).lpf(1200).sustain(0.85)
-  .gain(0.6).room(1.7)
+  .release(0.55).attack(0.1).lpf(1000).sustain(0.85)
+  .gain("<0.45 0.35>*2").room(1.7).decay(0.5)
 let backvocal = n(`<
 {~} 
 {~}
 {~ [f#4 e4] [f#4 e4] ~ ~ [f#4 e4] [f#4 e4@2 d4]@2}
 >`).sound("gm_violin").scale("C4:chromatic")
   .release(0.55).attack(0.1).lpf(600).sustain(0.85)
-  .gain(0.3).room(1.8).decay(0.7)
+  .gain("<0.3 0.15 0.3 0.15>*4").room(1.8).decay(0.7)
 $:
 stack(
   drums._punchcard(), 
