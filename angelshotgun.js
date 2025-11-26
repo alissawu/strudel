b508fc07733ece753711ba9d1f29124f0aa49d68
Alissa <alissawu77@gmail.com>
2025-11-26 05:41:33 -0500

    sounding decent intro

--- angelshotgun.js
+++ angelshotgun.js
@@ -1,13 +1,13 @@
-setcpm(30/4)
+setcpm(36/4)
 
 // notes
 // gm_tuba:0 (muffled tuba)
 // gm_fx_atmosphere(13):2 (i think?) - kinda angelic
 // gm_french_horn(5):1 and 5 (the last one. or is it 5?) (distant)
 //tr808 1, 6
-let drums = s("tr808_bd:6, [~ sd:1]*2")
+let drums = s("tr808_bd:1, tr808_bd:6")
   .struct(`<
-  { 1 [~@7 1] [1 ~] 1}
+  { 1 [~@7 1] 1 1}
   >`).gain(0.2).sustain(0.5).lpf(100)
 let bkgd = n(`<
   {f#4 g4 f#4 e4}
@@ -17,23 +17,50 @@ let bkgd = n(`<
   .lpenv(-3).pan(cosine.slow(2))
 let front = n(`<
 {~}
-  {~ [b3 b3, d4 d4, f#4 f#4, f#5 f#5] ~ [g3 g3, d4 d4, g4 g4, g5 g5] ~ [a3 a3, d4 d4, f#4 f#4, f#5 f#5] [a3, e4]@2}!2
->`).scale("C2:chromatic").sound("gm_voice_oohs:3").room(2).lpf(500)
-  .hpf(300).attack(0.3).release(1).sustain(1).decay(0.75).gain(0.15)
+  {~ [f#3 f#3, b3 b3, d4 d4, f#4 f#4, f#5 f#5] ~ [g2 g2, g3 g3, d4 d4, g4 g4, g5 g5] ~ [f#3 f#3, a3 a3, d4 d4, f#4 f#4, f#5 f#5] [e3, a3, e4]@2}
+  {[f#2 f#2] [f#3 f#3, b3 b3, d4 d4, f#4 f#4, f#5 f#5] [g2 g2] [g2 g2, g3 g3, d4 d4, g4 g4, g5 g5] [f#2 f#2] [f#3 f#3, a3 a3, d4 d4, f#4 f#4, f#5 f#5] [e3, a3, e4]@2}
+>`).scale("C2:chromatic").sound("gm_fx_atmosphere").room(2).lpf(500)
+  .hpf(300).attack(0.2).release(1).sustain(1).decay(0.75).gain("<0.1 0.1 0.05>")
   .lpenv(-3).pan(sine.slow(2))
 let bass = n(`<
-{b1 g1 d2 c#2}>`)
+{b1 g1 d2 c#2}!2
+{[[b1 b1] b1] [[g1 g1] g1] [[d2 d2] d2] [[a2 a2] a2, [c#2 c#2] c#2]}!2
+>`)
   .scale("C2:chromatic")
   .sound("supersaw").sustain(0.7)
   .release(0.1)
   .lpf("<1000 800 600 400>*2")
   .room(1)
-  .gain(0.05).resonance(0.6).pan(sine.slow(2))
+  .gain(0.05).pan(sine.slow(2))
+// its a bit more hoarse than this i think
 let brass = n(`<{a3 a3 a3 c#4}>`).sound("gm_french_horn").scale("C2:chromatic")
   .sustain(0.7)
   .release(0.1).lpf("<1000 800 600 400>*2")
   .room(1)
-  .gain(0.04).resonance(0.6).pan(sine.slow(2))
-$: stack(
-  brass, bkgd, front, bass, drums
+  .gain(0.04).pan(sine.slow(2))
+//{~} {~@6 [~@2 e4 e4] [f#4 e4 f#4 a4]}
+let vocal = n(`<
+{~} 
+{~@6 [~@2 e4 e4] [f#4 e4 f#4 a4]}
+{[f#4 e4] ~ ~ [f#4 e4 f#4 a4] [f#4 e4] ~ ~@2}
+>`).sound("gm_violin").scale("C4:chromatic")
+  .release(0.55).attack(0.1).lpf(1200).sustain(0.85)
+  .gain(0.6).room(1.7)
+let backvocal = n(`<
+{~} 
+{~}
+{~ [f#4 e4] [f#4 e4] ~ ~ [f#4 e4] [f#4 e4@2 d4]@2}
+>`).sound("gm_violin").scale("C4:chromatic")
+  .release(0.55).attack(0.1).lpf(600).sustain(0.85)
+  .gain(0.3).room(1.8).decay(0.7)
+$:
+stack(
+  drums._punchcard(), 
+  bkgd._punchcard(), 
+  front._punchcard(), 
+  bass._punchcard(), 
+  brass._punchcard(), 
+  vocal._punchcard(), 
+  backvocal._punchcard()
 )
+
