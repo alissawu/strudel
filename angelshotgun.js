4e92c1d5fe5dc2b91f0b96c2476edabeb55841bf
Alissa <alissawu77@gmail.com>
2025-11-26 22:46:45 -0500

    .

--- angelshotgun.js
+++ angelshotgun.js
@@ -8,10 +8,10 @@ setcpm(36/4)
 
 
 // intro
-let drums = s("tr808_bd:1, tr808_bd:6")
+let drums = s("tr808_bd:3, tr808_bd:6")
   .struct(`<
   { 1 [~@7 1] 1 1}
-  >`).gain("<0.3 0.2 0.2 0.2>*4").sustain(0.5).lpf(100)
+  >`).gain("<0.3 0.2 0.3 0.2>*4").sustain(0.5)
 let bkgd = n(`<
   {f#4 g4 f#4 e4}
   {[b3, f#4] [g3, g4] [d4, f#4] [a3, e4]}
@@ -34,7 +34,7 @@ let bass = n(`<
   .release(0.1)
   .lpf("<1000 800 600 400>*2")
   .room(1)
-  .gain(0.05).pan(sine.slow(2))
+  .gain(0.1).pan(sine.slow(2))
 // its a bit more hoarse than this i think
 let brass = n(`<{a3 a3 a3 c#4}>`).sound("gm_french_horn").scale("C2:chromatic")
   .sustain(0.7)
@@ -54,10 +54,10 @@ let backvocal = n(`<
 {~}
 {~ [f#4 e4] [f#4 e4] ~ ~ [f#4 e4] [f#4 e4@2 d4]@2}
 >`).sound("gm_violin").scale("C4:chromatic")
-  .release(0.4).attack(0.1).lpf(600).sustain(0.85)
-  .gain("<0.25 0.1 0.25 0.1>*4").room(1.5).decay(0.7)
+  .release(0.4).attack(0.1).lpf(1200).sustain(0.85)
+  .gain("<0.15 0.1 0.15 0.1>*4").room(1.5).decay(0.7)
 
-// verse 1
+// verse 1 get out your guns
 //{[~ d4] [d4@0.4 b3@0.6] d4@2 [~ d4] [d4@0.4 b3@0.6] d4@1.5 e4@0.2 ~@0.3 [~ d4] [d4@0.4 b3@0.6] [d4 e4] d4@0.5 d4 [c#4 b3] a3@1.95 b3@0.05}
 //{[~ d4] [d4@0.4 b3@0.6] d4@2 [~ d4] [d4@0.4 b3@0.6] d4@2 ~ [d4 b3] [d4 e4] d4@0.5 d4@1.5 c#4@1.5 d4@0.5 d4@0.5 d4@0.5}
 let verse1 = n(`<
@@ -68,6 +68,11 @@ let verse1 = n(`<
 >`).sound("gm_violin").scale("C4:chromatic")
   .release(0.3).attack(0.1).lpf(1500).sustain(0.85)
   .gain("0.15").room(1)
+let bkgd1 = n(`<
+  {f#4 g4 f#4 e4}
+>`).scale("C2:chromatic").sound("gm_pad_warm:6").room(2).lpf(500)
+  .hpf(300).attack(0.5).release(1.5).sustain(1).decay(0.75).gain(0.02)
+  .lpenv(-3).pan(cosine.slow(2))
 let bass1 = n(`<
 {[b1, b3] [g1, g3] [d2, f#2] [a1, c#2, c#3]}!2
 >`)
@@ -80,7 +85,7 @@ let bass1 = n(`<
 let drums1 = s("tr808_bd:1, [~ tr808_bd:6]*2")
   .struct(`<
   { [1 1] [[~ 1] 1]}
-  >`).gain("<0.4 0.2 0.3 0.2>*4").sustain(0.5).lpf(100).fast(4)
+  >`).gain("<0.4 0.2 0.3 0.2>*4").lpf(200).sustain(0.5).fast(4)
 let piano1 = n(`<
 {b3 [g3, d4, g2] [~@0.05 [a3, d4, f#3]@0.95] [[a3 c#4]@3 b3]}!2
 >`).scale("C2:chromatic").sound("gm_piano").attack(0.1).release(0.3).gain(0.1).room(1.8).lpf(2000)
@@ -88,16 +93,37 @@ let topbrass = n(`<
 {~}!2
 {b4 [b4@3 a4] [b4 d5] [b4 a4] b4 [b4@3 a4] [b4 d5] [e5 d5] e5 [e5@3 d5] [e5 f#5] [e5 d5] c#5 [c#5@3 b4] [c#5 d5] [c#5 a4]}
 {b4 [b4@3 a4] [b4 d5] [b4 a4] b4 [b4@3 a4] [b4 d5] [e5 d5] e5 [e5@3 d5] [e5 f#5] [e5 d5] c#5 [c#5@3 b4] c#5 ~}
->`).scale("C2:chromatic").sound("gm_contrabass").lpf(1500).gain(0.07)
+>`).scale("C2:chromatic").sound("gm_contrabass").lpf(1500).gain(0.1)
 //chorus -- "shotgun, fighting til the"
 //line 2: "faith babe just to"
 let chorus = n(`<
 {f#4 e4 [f#4 e4] [f#4 a4] f#4 e4 [f#4 e4] [f#4 e4] [d4 d4] d4 d4 d4@0.5 e4@1.5 e4@0.5 [f#4 e4] [f#4 a4]}
-{f#4 e4 [f#4 e4] [f#4 a4] f#4 [g4 f#4] [e4 d4] d4 d4@0.5 b4@1.5 a4@0.5 [a4 e4] [f#4 e4]
+{f#4 e4 [f#4 e4] [f#4 a4] f#4@2 [g4 f#4] [e4 d4] d4 d4@0.5 b4@1.5 a4@0.5 a4 e4@0.5 f#4@0.5 e4@2 d4@0.5}
 >`).sound("gm_violin").scale("C4:chromatic")
   .release(0.3).attack(0.1).lpf(1500).sustain(0.85)
-  .gain("0.15").room(1)
-
+  .gain(0.4).room(1)
+let bassch = n(`<
+{b1 b1 [b1 d2] [b1 a1] g1 g1 [g1 f#1] [g1 a1] d2 d2 d2 f#2 a1 [a1 e1] a1 c#2}!2
+>`)
+  .scale("C2:chromatic")
+  .sound("supersaw").sustain(0.7)
+  .release(0.1)
+  .lpf("900")
+  .room(1).gain(0.3)
+  .gain(0.05).pan(sine.slow(2))
+let vocalch = n(`<
+{[b4, d5] [g4, b4, g5] [a4, d5, f#4] [c#5, e5]}
+>`).scale("C2:chromatic").sound("gm_pad_warm:6").room(2).lpf(500)
+  .hpf(300).attack(0.5).release(1.5).sustain(1).decay(0.75).gain(0.2)
+  .lpenv(-3).pan(cosine.slow(2))
+// 2, 5, 6 are candidates
+let guitarch = n(`<
+{[b2 b2] [b2 b2 b2 b2] [g2 g2] [g2 g2 g2 g2] [d3 d3] [d3 d3 d3 d3] [a2 a2] [a2 a2 c#3 c#3]}>`)
+  .scale("C2:chromatic").sound("gm_overdriven_guitar:6").room(1.5).lpf(800).gain(0.3).attack(0.1).release(0.5)
+let drumsch = s("[~ dmx_sd:3]*2, [tr808_bd:6 ~]*2")
+  .struct(`<
+  { 1 [1@3 1] [1 1] 1}
+  >`).gain("<0.3 0.1 0.2 0.1>*4").sustain(0.7).release(0.3).fast(4).postgain(0.2)
 
 
 arrange(
@@ -109,8 +135,14 @@ arrange(
             vocal._punchcard(), 
             backvocal._punchcard())],
   [4, stack(verse1._punchcard(), 
+            bkgd1._punchcard(),
             bass1._punchcard(), 
             drums1._punchcard(), 
             piano1._punchcard(), 
-            topbrass._punchcard())]
+            topbrass._punchcard())],
+  [2, stack(chorus._punchcard(),
+            bassch._punchcard(),
+            vocalch._punchcard(),
+            guitarch._punchcard(),
+            drumsch._punchcard())]
 ).theme("sonicPink")
