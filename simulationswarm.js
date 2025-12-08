ff77adf403cd0437cb25cf59784eeb8f3df87333
Alissa <alissawu77@gmail.com>
2025-12-08 18:57:34 -0500

    will try to fix alignment later. added bass

--- simulationswarm.js
+++ simulationswarm.js
@@ -14,8 +14,17 @@ let guitar = n(`<
   .scale("C2:chromatic")
   .sound("gm_acoustic_guitar_nylon").sustain(0.3).release(0.5)
 
+//delay 6 of 8 beats, so 0.75. slow(2) => 0.375
+//each note is approx 4 beats, so
+let bass = n(`<
+{~}@0.375
+{c#2 f#1 g#1 f#1}!10
+>`)
+  .scale("C2:chromatic")
+  .sound("gm_acoustic_guitar_nylon").release(1).gain(0.1).slow(2)
+
 $: stack(
-  drums, guitar
+  drums, guitar, bass
 )
 
 
