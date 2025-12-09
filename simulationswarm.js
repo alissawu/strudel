66bfababb30ca73c7a8667fbf0f86aa3c23cb814
Alissa <alissawu77@gmail.com>
2025-12-08 19:02:58 -0500

    fixed alginemnt but like bandaid

--- simulationswarm.js
+++ simulationswarm.js
@@ -3,13 +3,16 @@ setcpm(120/4)
 // intro
 // the 1 1 1 1 lowk repeats forever, the first line is just the intro thing
 let drums = n(`<
-  {~@2 [1 [1 1]] 1}
+  {~@3 [1 [1 1]]}
   { 1 1 1 1}!32
   >`).sound("[dmx_sd:3 ~]*2, [~ akaixr10_bd:2]*2").gain(0.05)
 
+//drums start at the 6th of 8 beats
+//so we need 1.5 more beats. 1.5/8 = 
 let guitar = n(`<
-{~@6 c#4 f4}
-{bf3@0.5 c4 c#4@1.5 f4 bf3@0.5 c4 c#4@1.5 f4}!32
+{~}@0.1875
+{~@4.5 c#4 f4 bf3@0.5 c4}
+{c#4@1.5 f4 bf3@0.5 c4 c#4@1.5 f4 bf3@0.5 c4}!32
 >`)
   .scale("C2:chromatic")
   .sound("gm_acoustic_guitar_nylon").sustain(0.3).release(0.5)
